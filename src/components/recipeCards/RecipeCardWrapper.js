60a7173f1e3c546c4cb66ce02e6d99e7efe5be8d
Vim <Vim@vimals.local>
2020-09-06 02:24:31 +0530

    part5 committing

--- src/components/recipeCards/RecipeCardWrapper.js
+++ src/components/recipeCards/RecipeCardWrapper.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RecipeCard from "./RecipeCard";
 import "./RecipeCards.css";
 
+const API_KEY = "fc754827722b42b49893f499c1a4839d";
+
 let RecipeCardWrapper = (props) => {
-  console.log(props);
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    if (props.selectedRecipe) fetchRecipe();
+  }, [props.selectedRecipe]);
+
+  let fetchRecipe = async () => {
+    let response = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&cuisine=${props.selectedRecipe}&addRecipeInformation=true&number=6`
+    );
+    let recipeData = await response.json();
+    setRecipes(recipeData.results);
+  };
+
   return (
     <div className="recipeWrapper">
-      <RecipeCard />
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.id} recipe={recipe} />
+      ))}
     </div>
   );
 };
